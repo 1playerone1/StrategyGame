5535bc08911635926148ae966c9dc484a2ac9baa
1playerone1 <playerone1923@gmail.com>
2023-03-05 23:04:45 +0300

    added camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 10f;
+    [SerializeField] private float _rotationSpeed = 70f;
+
+    private void Update()
+    {
+        Vector3 inputMoveDir = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMoveDir.z = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputMoveDir.z = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMoveDir.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveDir.x = +1f;
+        }
+
+        float moveSpeed = _moveSpeed;
+
+        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+
+        Vector3 rotationVector = new Vector3 (0, 0, 0);
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationVector.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationVector.y = -1f;
+        }
+
+        float rotationSpeed = _rotationSpeed;
+
+        transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
+}
